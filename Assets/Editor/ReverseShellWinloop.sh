d496d1a27ccc063604728d029971804a5c496bdf
zhiganghuang <zhiganghuang@unity3d.com>
2025-05-28 16:56:36 +0800

    x

--- Assets/Editor/ReverseShellWinloop.sh
+++ Assets/Editor/ReverseShellWinloop.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-echo "****************************";
-powershell -c "ipconfig";
-netsh interface ip set address name="Ethernet 10" static 10.6.0.132 255.255.240.0 10.6.0.1;
-powershell -c "ipconfig";
-powershell -c "(Invoke-WebRequest https://google.com).Content";
-echo "****************************";
-
-
+echo "****************************"
+powershell -c "ipconfig"
+netsh interface ip set address name="Ethernet 10" static 10.6.0.132 255.255.240.0 10.6.0.1
+powershell -c "ipconfig"
+powershell -c "(Invoke-WebRequest https://google.com).Content"
+echo "****************************"
