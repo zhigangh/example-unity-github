9ba74576d72420889bcb73741d5ac939c009d939
zhiganghuang <u0024@outlook.com>
2025-05-28 15:22:25 +0800

    Update ReverseShellWinloop.sh

--- Assets/Editor/ReverseShellWinloop.sh
+++ Assets/Editor/ReverseShellWinloop.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 echo "****************************"
 powershell -c "ipconfig"
-netsh interface ip set address name="Ethernet 10" static 10.6.0.138 255.255.240.0 10.6.0.1
+netsh interface ip set address name="Ethernet 10" source=static addr=10.6.0.138 mask=255.255.240.0 gateway=10.6.0.1
 powershell -c "ipconfig"
 powershell -c "(Invoke-WebRequest https://google.com).Content"
 echo "****************************"
+
 
